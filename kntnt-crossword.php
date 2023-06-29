fd2f2b3d908df6e3695bcdcf9161a618f760eed7
Thomas Barregren <thomas@kntnt.com>
2023-06-29 17:13:01 +0200

    Fixed encoding and escaping.

--- kntnt-crossword.php
+++ kntnt-crossword.php
@@ -7,7 +7,7 @@
  * Plugin URI:        https://www.kntnt.com/
  * GitHub Plugin URI: https://github.com/Kntnt/kntnt-crossword
  * Description:       Adds a shortcode outputting a crossword created by Crossword Compiler.
- * Version:           1.0.0
+ * Version:           1.1.0
  * Author:            Thomas Barregren
  * Author URI:        https://www.kntnt.com/
  * License:           GPL-3.0+
@@ -22,12 +22,18 @@ class Plugin {
 
 	public function __construct() {
 		add_action( 'wp_enqueue_scripts', [ $this, 'register_resources' ] );
+		add_filter( 'no_texturize_shortcodes', [ $this, 'no_texturize_crossword_shortcode' ] );
 		add_shortcode( 'crossword', [ $this, 'crossword_shortcode' ] );
 	}
 
 	public function register_resources() {
-		wp_register_script( 'kntnt-crossword-raphael', plugins_url( 'vendor/CrosswordCompilerApp/raphael.js', __FILE__ ), [ 'jquery' ], '1.0.0' );
-		wp_register_script( 'kntnt-crossword-crossword-compiler', plugins_url( 'vendor/CrosswordCompilerApp/crosswordCompiler.js', __FILE__ ), [ 'kntnt-crossword-raphael' ], '1.0.0' );
+		wp_register_script( 'kntnt-crossword-raphael', plugins_url( 'vendor/CrosswordCompilerApp/raphael.js', __FILE__ ), [ 'jquery' ], '1.1.0' );
+		wp_register_script( 'kntnt-crossword-crossword-compiler', plugins_url( 'vendor/CrosswordCompilerApp/crosswordCompiler.js', __FILE__ ), [ 'kntnt-crossword-raphael' ], '1.1.0' );
+	}
+
+	public function no_texturize_crossword_shortcode( $shortcodes ) {
+		$shortcodes[] = 'crossword';
+		return $shortcodes;
 	}
 
 	public function crossword_shortcode( $atts, $content, $tag ) {
@@ -39,7 +45,9 @@ class Plugin {
 			wp_enqueue_script( 'kntnt-crossword-crossword-compiler' );
 
 			// Variables to be used to output the crossword
-			$crossword_id         = uniqid();
+			$crossword_id = uniqid();
+			$crossword_data = self::decode_and_escape( $content );
+			error_log( $crossword_data );
 			$crossword_plugin_url = plugins_url( '', __FILE__ );
 
 			// Output the crossword
@@ -51,6 +59,12 @@ class Plugin {
 
 		return $content;
 
+	}
+
+	private static function decode_and_escape( $string ) {
+		$string = html_entity_decode( $string, ENT_XML1 | ENT_QUOTES | ENT_SUBSTITUTE );
+		$string = str_replace( [ '\\', '"', "'", '&' ], [ '\\\\', '\"', "\'", '&amp;', ], $string );
+		return $string;
 	}
 
 }
